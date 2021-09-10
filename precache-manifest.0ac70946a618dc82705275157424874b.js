26e72ff459f47c8f4b476403673f122930a7fabd
Benjamin <benjamincook1313@gmail.com>
2021-09-10 17:21:32 -0600

    Updates

---
+++ precache-manifest.0ac70946a618dc82705275157424874b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d8660ba7d1036cc53786eab6aec334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a921082097d28b1f676f",
+    "url": "/static/css/main.1e0716ef.chunk.css"
+  },
+  {
+    "revision": "1a0004a6d91a79049653",
+    "url": "/static/js/2.19037096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19037096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a921082097d28b1f676f",
+    "url": "/static/js/main.7fd3cd66.chunk.js"
+  },
+  {
+    "revision": "7d55e047f81b2d309549",
+    "url": "/static/js/runtime-main.2581e6bc.js"
+  }
+]);
